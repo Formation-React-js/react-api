b95a864492e5d373de7a8a0f5875f5a38411bfd5
Romain Tello <romain.tello@gmail.com>
2020-05-26 14:36:50 +0200

    Add routing for people

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Person } from './components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <Person />
+    <BrowserRouter>
+      <Container>
+        <Switch>
+          <Route exact path="/person/:id" render={
+            ({ match }) => <Person id={match.params.id} />
+          } />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 }
 
--- src/components/person/index.js
+++ src/components/person/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, Spinner } from 'react-bootstrap';
 
 class Person extends Component {
   state = {
@@ -6,7 +7,9 @@ class Person extends Component {
   }
 
   componentDidMount = () => {
-    fetch('https://swapi.dev/api/people/1')
+    const { id } = this.props;
+
+    fetch(`https://swapi.dev/api/people/${id}`)
     .then(response => response.json())
     .then(data => this.setState({ person: data }));  
   }
@@ -15,13 +18,21 @@ class Person extends Component {
     const { person } = this.state;
 
     if (person === null) {
-      return <div>Loading...</div>
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
     }
 
     return (
-      <div>
-        <h1>{person.name}</h1>
-      </div>
+      <Card>
+        <Card.Header as="h3">{person.name}</Card.Header>
+        <Card.Body>
+          <Card.Text>Height: {person.height}</Card.Text>
+          <Card.Text>Mass: {person.mass}</Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
 }
