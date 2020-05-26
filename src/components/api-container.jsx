5f6152da803beed0d18634777d8fd658490f86d6
Romain Tello <romain.tello@gmail.com>
2020-05-26 17:24:54 +0200

    Add comments

--- src/components/api-container.jsx
+++ src/components/api-container.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Spinner } from 'react-bootstrap';
 
+// Ce composant est responsable d'aller chercher les données dans l'API
+// et affiche le rendu normal du composant qui l'a appelé uniquement
+// une fois que celles-ci sont arrivées
 class ApiContainer extends Component {
   state = {
     data: null,
   }
 
+  // Au moment où le composant est créé, envoie une requête à l'API
+  // et stocke le résultat dans son state
   componentDidMount = () => {
     const { type, id } = this.props;
 
@@ -18,6 +23,7 @@ class ApiContainer extends Component {
     const { children } = this.props;
     const { data } = this.state;
 
+    // Tant que la requête à l'API n'a pas répondu, afficher un loader
     if (data === null) {
       return (
         <Spinner animation="border" role="status">
@@ -26,6 +32,9 @@ class ApiContainer extends Component {
       );
     }
 
+    // Dès que la requête a répondu, renvoie l'affichage normal du
+    // composant supérieur, en lui passant les données de l'API
+    // comme paramètre
     return children(data);
   }
 }
